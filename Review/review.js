cb0f73701579044ca1248ea501533f304a90cfed
Nate Crawford <nathanwcrawford22@gmail.com>
2022-08-18 15:24:23 -0400

    Booleans

--- Review/review.js
+++ Review/review.js
@@ -16,11 +16,30 @@
 //3.
     firstVariable = 9;
 //4. 
-    secondVariable = firstVariable;
+    let secondVariable = firstVariable;
 //5.
     secondVariable = 'Goodnight World';
 //6.
-     9   //console.log(firstVariable);
+     console.log(firstVariable);
 //7. 
    let yourName = 'Nate';
    console.log('Hello, my name is ' + yourName) // Hello, my name is Nate
+
+// C. Booleans
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16; 
+const e = 'Kevin';
+
+console.log(a != b);
+console.log(c > d);
+console.log('Name' == 'Name');
+// Only use && or || for the next two
+console.log(true || false);
+console.log(false || false && false || false && false || true );
+console.log(false == false);
+console.log(e === 'Kevin');
+console.log(a + b === c);
+console.log(a * a === d);
+console.log(48 =='48');
