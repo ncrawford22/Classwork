5d321c321a7a312e6594e478217bdd94714c3afc
Nate Crawford <nathanwcrawford22@gmail.com>
2022-08-18 14:07:41 -0400

    Updated Classes Exercises v2

--- Classes/classes.js
+++ Classes/classes.js
@@ -347,3 +347,33 @@
 //   lucas.withdraw(1000)
 //   usman.withdraw(2000000)
 //   stephin.withdraw(4) 
+
+// ----------------------------------------------------------------------------------------------------------------------------- //
+
+// Singleton Pattern & Method chaining (Do research!)
+class Car {
+    constructor (maker, serialNumber) {
+      this.maker = maker;
+      this.serialNumber = serialNumber;
+    }
+    drive () {
+      console.log('Vroom Vroom');
+    }
+  }
+  class Factory {
+    constructor(company) {
+      this.company = company;
+      this.cars = [];
+    }
+    generateCar () {
+      const newCar = new Car(this.company, this.cars.length);
+      this.cars.push(newCar);
+      return this
+    }
+    findCar(index) {
+      return this.cars[index];
+    }
+  }
+  // only need one instance (Singleton)
+  const tesla = new Factory('Tesla')
+
