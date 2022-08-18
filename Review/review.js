088c3e574248e460a1cde8ff0bf7216654ca3cf0
Nate Crawford <nathanwcrawford22@gmail.com>
2022-08-18 14:41:33 -0400

    Variable & Datatypes

---
+++ Review/review.js
@@ -0,0 +1,9 @@
+// Variables & Datatypes
+
+// A. Q + A
+//1. You can assign a value to a variable using the = operator after you have declared it.
+//2. We can change the value of a variable by referencing it, followed by using the assignment operator and entering a new value.
+//3. We reference the existing variable, followed by the assignment operator and then insert the new variable.
+//4. Declare: Variable is registered a given name within the cooresponding; Assign: Assigns a value to a variable; Define: Using a keyword to 'declare" i.e. "function doTest"
+//5. Pseudocode is a way of writing programs structured like a set of instructions written out in sentences that just about any human can read and comprehend. 
+//6. 95% thinking about how to solve vs 5% actually typing the code
